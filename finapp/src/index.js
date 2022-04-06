f6f5472f5145aace31e356c9d48d44dfd44d3eed
Bruno Vieira <vieira.es@gmail.com>
2022-04-05 21:45:35 -0300

    feature/added fully API on bank flow

--- finapp/src/index.js
+++ finapp/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const crypto = require('crypto');
 
 
 
@@ -7,38 +8,52 @@ const app = express();
 // Create a new express application instance
 
 
-//middleware of express
-app.use(express.json());
+app.use(express.json())
+//middleware do express para aceitar json
 
 
-// middleware
-function verifyIfExistAccountCPF(request, response, next) {
-    const { cpf } = request.headers;
-    const customer = customers.find(customer => customer.cpf === cpf);
 
+
+
+const customersDB = [];
+// {
+//     accountId: accountId,
+//     wallet: walletCode,
+//     name,
+//     cpf,
+//     created_at: new Date(),
+//     statements: statementsDB,
+//     balance: getBalance(statementsDB)
+// }
+
+
+const statementsDB = [];
+// {
+//     amount,
+//     description,
+//     created_at: new Date(),
+//     transactionType: [],
+// }
+
+const walletsDB = [];
+
+
+
+
+function verifyIfExistsAccountCPFInParams(request, response, next) {
+    const { cpf } = request.params;
+    const customer = customersDB.find(customer => customer.cpf === cpf);
 
 
 
     if (!customer) {
-        return response.status(400).json({ error: "Customer CPF not found" });
-
+        return response.status(400).json({
+            error: "Customer CPF not found"
+        });
     }
 
     request.customer = customer;
-
-
-    return next();
-
-
-}
-
-
-function verifyExistsWallet(request, response, next) {
-
-    const { wallet } = request.params;
-
-    const customer = customers.find(customer => customer.wallet === wallet);
-
+    // esta linha garante que o customer será acessível em todas as funções que estiverem dentro do middleware
     return next();
 };
 
@@ -46,132 +61,168 @@ function verifyExistsWallet(request, response, next) {
 
 
 
-const customers = [];
-
 
 
 
 app.post("/account", (request, response) => {
 
 
-    const { cpf, name, wallet } = request.body;
-    const costumerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+    const { cpf, name } = request.body;
+
+    const accountCode = uuidv4();
+    const walletCode = crypto.randomBytes(8).toString('HEX');
+
+
+
+    const costumerCPFAlreadyExists = customersDB.some((customerCPF) => customerCPF.cpf === cpf);
     // o some retorna true ou false
-
-
-    if (costumerAlreadyExists === true) {
-        return response.status(400).json({ error: "Customer already exists" });
+    if (costumerCPFAlreadyExists === true) {
+        return response.status(400).json({
+            cpf: cpf,
+            message: "Error. Customer CPF already exists"
+        });
 
     }
 
 
-    customers.push(
+    const objetoResponse = {
+        accountId: accountCode,
+        name: name,
+        cpf: cpf,
+        wallet: walletCode,
+        created_at: new Date(),
+        balance: 0
+    };
+
+
+    // gravar no array
+    customersDB.push(
         {
-            id: uuidv4(),
-            name,
-            cpf,
-            wallet,
-            statement: [] 
-            //statement.sort((a, b) => a.date - b.date)
-            
+            accountId: objetoResponse.accountId,
+            name: objetoResponse.name,
+            cpf: objetoResponse.cpf,
+            wallet: objetoResponse.wallet,
+            balance: objetoResponse.balance,
+            created_at: objetoResponse.created_at,
+            statements: []
         }
-
-
-
     );
 
-    return response.status(200).send({ message: "Customer created" });
 
 
-});
-
-app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
-
-
-    const { amount, description, created_at } = request.body;
-    // os dados que EU QUERO pegar do corpo da requisição
-
-    //how to convert amount to a positive value
-
-    const { customer } = request;
-
-    // os dados que eu quero pegar do request
-
-
-
-    const statementOperation = {
-        amount,
-        description,
-        created_at: new Date(),
-        type: "deposit",
-
-    };
-    //os dados que eu que seja adicionado no statement
-
-
-
-    customer.statement.push(statementOperation);
-    //ler assim: empurra uma statementOperation no statement do customer
-    // push adiciona um elemento no array na posição especificada
-
-
-
-    return response.status(200).json({
-        message: "Deposit success",
-        type: statementOperation.type,
-        amount: statementOperation.amount,
-        balance: getBalance(customer.statement),
-        created_at: statementOperation.created_at
+    return response.status(200).send({
+        message: "Account created",
+        accountId: objetoResponse.accountId,
+        wallet: objetoResponse.wallet,
+        name: objetoResponse.name,
+        cpf: objetoResponse.cpf,
+        created_at: objetoResponse.created_at,
     });
 
 
 });
 
 
-app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
 
-    const { amount, description, created_at } = request.body;
-    //informacao que vem do body
+// app.get("/wallets", (request, response) => {
 
+
+
+//     return response.status(200).json({
+//         message: "all wallets listed",
+//         walleta: [{
+
+//             walletId: customersDB.wallet,
+//             created_at: customersDB.created_at,
+//             balance: customersDB.balance,
+//             statements: [{
+//                 statementId: customersDB.statements,
+//                 transactionType: customersDB.transactionType,
+//                 description: customersDB.description,
+//                 created_at: customersDB.created_at,
+//             }]
+//         }]
+
+
+
+//     })
+// });
+
+
+
+//LISTANDO TODAS AS CONTAS
+app.get("/accounts", (request, response) => {
     const { customer } = request;
-    //informacao que vem do request
-
-    const balance = getBalance(customer.statement);
 
 
-    if (balance < amount) {
-        return response.status(400).json({ error: "Insufficient balance" });
-    }
+    const accounts = customersDB.map(customer => {
 
 
-    const statementOperation = {
-        amount,
-        description,
-        created_at: new Date(),
-        type: "withdraw"
+        // o que faz o map?
+        // ele vai percorrer o array e vai retornar um novo array com os dados que queremos
+        return {
+            name: customer.name,
+            cpf: customer.cpf,
+            wallet: customer.wallet,
+            accountId: customer.accountId,
+            created_at: customer.created_at,
+            balance: getBalance(customer.statements)
+        };
+    });
 
-    };
-
-
-    customer.statement.push(statementOperation);
 
     return response.status(200).json({
-        message: "Withdraw success",
-        type: statementOperation.type,
-        amount: statementOperation.amount,
-        balance: getBalance(customer.statement),
-        created_at: statementOperation.created_at,
+
+        resume: {
+            length: customersDB.length,
+            accounts: (accounts.sort((a, b) => b.created_at - a.created_at))
+
+        },
     });
 });
+
+
+
+
+
+
+
+
+
+
+//LISTANDO STATEMENTS DE UMA CONTA
+app.get("/statements/:cpf", verifyIfExistsAccountCPFInParams, (request, response) => {
+
+
+    const { customer } = request;
+    // esta linha esta recuperando o customer que está dentro do middleware declarado
+
+
+    return response.status(200).json({
+        balance: getBalance(customer.statements),
+        statements: customer.statements.sort((a, b) => b.created_at - a.created_at)
+    });
+});
+
+
+
+
+
+
+
 
 
 function getBalance(statement) {
-    var balance = statement.reduce((acc, operation) => {
-        if (operation.type === 'deposit') {
+    const balance = statement.reduce((acc, operation) => {
+
+        if (operation.transactionType === 'deposit') {
             return acc + operation.amount;
-        } else {
+        } else if (operation.transactionType === 'withdraw') {
             return acc - operation.amount;
+        } else {
+            return acc;
         }
+
     }, 0);
     // o 0 significa que o valor inicial é 0
 
@@ -181,40 +232,100 @@ function getBalance(statement) {
 
 
 
-//app.user(verifyIfExistAccountCPF);
-// insere um middleware em todas as rotas, usaro somente para isso
 
 
-app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
 
+//CRIANDO UMA TRANSAÇÃO DE TIPO DEPOSIT
+app.post("/deposit/:cpf", verifyIfExistsAccountCPFInParams, (request, response) => {
+
+    const { customer } = request;// esta linha esta recuperando o customer que está dentro do middleware declarado
+    const { amount, description } = request.body;
+
+
+
+    const statementOperation = {
+        amount,
+        description,
+        created_at: new Date(),
+        transactionType: 'deposit'
+    };
+
+    customer.statements.push(statementOperation);
+
+    return response.status(200).json({
+        message: "Deposit success",
+        type: statementOperation.transactionType,
+        amount: statementOperation.amount,
+        balance: getBalance(customer.statements),
+        created_at: statementOperation.created_at
+    });
+});
+
+
+
+
+
+
+
+
+
+//LISTANDO O SALDO DA CONTA
+app.get("/balance/:cpf", verifyIfExistsAccountCPFInParams, (request, response) => {
     const { customer } = request;
-    //informacao que vem do body
+
 
     return response.status(200).json({
         message: "Net balance",
-        balance: getBalance(customer.statement)
+        balance: getBalance(customer.statements)
     });
 
 });
 
 
-app.get("/account-statements", verifyIfExistAccountCPF, (request, response) => {
 
-    
-    
+
+//CRIANDO UMA TRANSAÇÃO DE TIPO WITHDRAW
+app.post("/withdraw/:cpf", verifyIfExistsAccountCPFInParams, (request, response) => {
+
+    const { amount, description } = request.body;
     const { customer } = request;
 
+
+
+
+
+    const balance = getBalance(customer.statements);
+
+
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient balance" });
+    }
+
+
+
+    const statementOperation = {
+        amount,
+        description,
+        created_at: new Date(),
+        transactionType: 'withdraw'
+
+    };
+
+
+
+
+
+    customer.statements.push(statementOperation);
+
     return response.status(200).json({
-        statements: customer.statement.sort((a, b) => b.created_at - a.created_at)
-       
+        message: "Withdraw success",
+        transactionType: statementOperation.transactionType,
+        amount: statementOperation.amount,
+        balance: getBalance(customer.statements),
+        created_at: statementOperation.created_at,
     });
-    
-
-    
-
-
-
-})
+});
 
 
 
@@ -223,6 +334,48 @@ app.get("/account-statements", verifyIfExistAccountCPF, (request, response) => {
 
 
 
+//#################### MIDDLEWARE
 
 
+
+
+
+function verifyExistsWallet(request, response, next) {
+
+    const customerCPF = customersDB.find(customer => customer.cpf === request.params.cpf);
+
+    const customerWallet = customersDB.find(customer => customer.wallet === request.body.customerCPF)
+
+    if (!customerWallet) {
+        return response.status(400).json({
+            message: "Error. Wallet not found"
+        })
+    }
+
+
+
+    return next();
+}
+
+
+
+function verifyValidRoute(request, response, next) {
+
+
+    const routeUrl = request.url;
+    const statusCode = request.statusCode;
+
+    if (statusCode === 404 || routeUrl !== '/') {
+
+        return response.status(404).json({ error: "Route not found" });
+    }
+    return next();
+};
+
+
+
+
+
+
+// PORTA DO APP
 app.listen(3000);
